19c4b367fd6bb2c5dce6745effff0444d234b7ac
Oscar Guerrero <>
2023-03-08 22:25:50 +0100

    fixed live connection

--- src/infra/repo/index.ts
+++ src/infra/repo/index.ts
@@ -8,4 +8,6 @@ import { LiveConnection } from "./liveConnection";
 import { TestConnection } from "./testConnection";
 
 export const Database =
-  EnvVars.NODE_ENV === "test" ? TestConnection : LiveConnection;
+  EnvVars.NODE_ENV === "test" || EnvVars.NODE_ENV === "development"
+    ? TestConnection
+    : LiveConnection;
